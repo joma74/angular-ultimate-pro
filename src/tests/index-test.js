c3c8df64b6d71dc71febb894ce491fd0e27fa734
joma74 <dev-mails@gmx.net>
2018-10-10 14:20:47 +0200

    test: test of heading

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -1,5 +1,6 @@
 // @ts-ignore
 import { AngularSelector, waitForAngular } from "testcafe-angular-selectors"
+var Mustache = require("mustache")
 
 const fixtureName = "Index_Page_Test"
 
@@ -14,4 +15,16 @@ const testName = "dom_has_critical_elements"
 test(testName, async (t) => {
   // await t.debug()
   await t.takeScreenshot()
+  const heading = AngularSelector().find("h1[data-desc='heading']")
+  const headingText = await heading.innerText
+  const expected = Mustache.render(
+    "{{ title }} from Angular App with Webpack {{ major }}.{{ minor }}.{{ patch }}",
+    {
+      major: "3",
+      minor: "12",
+      patch: "0",
+      title: "Hello",
+    },
+  )
+  await t.expect(headingText).eql(expected)
 })
