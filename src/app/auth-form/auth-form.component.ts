bee3dd357fcb811b217385263464ef7f23ca9d8a
joma74 <dev-mails@gmx.net>
2018-10-21 16:54:35 +0200

    feat: setup components for content projection, the case :)

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -7,7 +7,7 @@ import { User } from "./auth-form.interface"
   template: `
 	<div class="no-m-coll">
 	  <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
-		<ng-content></ng-content>
+		<h3>My Form</h3>
 		<div class="mt-4">
 			<label class="block text-grey-darker text-sm font-bold mb-2">
 				Email address
