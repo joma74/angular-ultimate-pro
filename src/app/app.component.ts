d9fca524280eaecb5c727f9b62f6d3a92d98ca0e
joma74 <dev-mails@gmx.net>
2018-10-25 17:27:50 +0200

    feat: halfway of adding destroy component action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {
   AfterContentInit,
   Component,
   ComponentFactoryResolver,
+  ComponentRef,
   ViewChild,
   ViewContainerRef,
 } from "@angular/core"
@@ -13,7 +14,11 @@ import "../assets/css/styles.css"
 @Component({
   selector: "app-root",
   template: `
-	<div class="container mx-auto bg-grey-lighter mt-10 p-8 shadow-md">
+    <div class="container mx-auto bg-grey-lighter mt-10 p-8 shadow-md">
+        <button (click)="destroyComponent()"
+            class="mb-4 bg-red hover:bg-red-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Destroy me
+        </button>
         <div #entry (submitted)='loginUser($event)'>
         </div>
 	</div>
@@ -23,19 +28,26 @@ export class AppComponent implements AfterContentInit {
   @ViewChild("entry", { read: ViewContainerRef })
   public entry: ViewContainerRef
 
+  public component: ComponentRef<AuthFormComponent>
+
   constructor(private resolver: ComponentFactoryResolver) {}
 
   public ngAfterContentInit(): void {
     const authFormFactory = this.resolver.resolveComponentFactory(
       AuthFormComponent,
     )
-    const component = this.entry.createComponent(authFormFactory)
-    component.instance.title = "Login"
-    component.instance.submitted.subscribe(this.loginUser)
+    this.component = this.entry.createComponent(authFormFactory)
+    this.component.instance.title = "Login"
+    this.component.instance.submitted.subscribe(this.loginUser)
   }
 
   public loginUser(user: User) {
     // tslint:disable-next-line:no-console
     console.log("Login", JSON.stringify(user))
+  }
+
+  public destroyComponent() {
+    // tslint:disable-next-line:no-console
+    console.log(this.component)
   }
 }
