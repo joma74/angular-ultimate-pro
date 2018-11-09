9f2854bb835e32d25c0f32759e640acfdbabe069
joma74 <dev-mails@gmx.net>
2018-11-10 00:12:48 +0100

    feat: formgroups added

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,10 +1,44 @@
 import { Component } from "@angular/core"
+import { FormArray, FormControl, FormGroup } from "@angular/forms"
 
 @Component({
   selector: "stock-inventory",
   styleUrls: ["stock-inventory.component.scss"],
   template: `
-    <div class="">Hello world!</div>
+    <div class="stock-inventory">
+      <form [formGroup]="form" (ngSubmit)="onSubmit()">
+        <div formGroupName="store">
+          <!-- display: block -->
+          <input type="text" placeholder="Branch ID" formControlName="branch" />
+          <input
+            type="text"
+            placeholder="Manager Code"
+            formControlName="code"
+          />
+        </div>
+        <div class="store-inventory__buttons">
+          <button type="submit" [disabled]="form.invalid">Order stock</button>
+        </div>
+        <pre>{{ form.value | json }}</pre>
+      </form>
+    </div>
   `,
 })
-export class StockInventoryComponent {}
+export class StockInventoryComponent {
+  public form = new FormGroup({
+    selector: new FormGroup({
+      product_id: new FormControl(""),
+      quantity: new FormControl(10),
+    }),
+    stock: new FormArray([]),
+    store: new FormGroup({
+      branch: new FormControl(""),
+      code: new FormControl(""),
+    }),
+  })
+
+  public onSubmit() {
+    // tslint:disable-next-line:no-console
+    console.log("Submit:", this.form.value)
+  }
+}
