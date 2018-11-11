0cae49528c9c86bc7d294d46384af3ffa33b8851
joma74 <dev-mails@gmx.net>
2018-11-12 00:25:40 +0100

    test: setup minimal test

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -1,6 +1,5 @@
 // @ts-ignore
-import { AngularSelector, waitForAngular } from "testcafe-angular-selectors"
-var Mustache = require("mustache")
+import { waitForAngular } from "testcafe-angular-selectors"
 
 const fixtureName = "Index_Page_Test"
 
@@ -10,38 +9,8 @@ fixture(fixtureName)
     await waitForAngular()
   })
 
-const testName = "dom_has_critical_elements"
+const testName = "can_open_page"
 
 test(testName, async (t) => {
   await t.takeScreenshot()
-  await checkHeading(t)
-
-  const imageAngularFirst = await AngularSelector().find(
-    "img[data-desc='angular-first']",
-  )
-  await t.expect(imageAngularFirst.visible).ok()
-
-  const imageAngularSecond = await AngularSelector().find(
-    "img[data-desc='angular-second']",
-  )
-  await t.expect(imageAngularSecond.visible).ok()
 })
-
-/**
- *
- * @param {TestController} t
- */
-async function checkHeading(t) {
-  const heading = AngularSelector().find("h1[data-desc='heading']")
-  const headingText = await heading.innerText
-  const expected = Mustache.render(
-    "{{ title }} from Angular App with Webpack {{ major }}.{{ minor }}.{{ patch }}",
-    {
-      major: "3",
-      minor: "12",
-      patch: "0",
-      title: "Hello",
-    },
-  )
-  await t.expect(headingText).eql(expected)
-}
