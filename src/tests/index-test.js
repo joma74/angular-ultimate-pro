0d1b0c4e9b814227f89b00c00e6318a13a9b4a3c
joma74 <dev-mails@gmx.net>
2018-11-13 23:12:32 +0100

    test: expect add stock button to be visible

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -1,10 +1,11 @@
 // @ts-ignore
 import { waitForAngular } from "testcafe-angular-selectors"
+import { Selector } from "testcafe"
 
 const fixtureName = "Index_Page_Test"
 
 fixture(fixtureName)
-  .page("http://localhost:4001/index.html")
+  .page("http://0.0.0.0:4001/index.html")
   .beforeEach(async () => {
     await waitForAngular()
   })
@@ -13,4 +14,10 @@ const testName = "can_open_page"
 
 test(testName, async (t) => {
   await t.takeScreenshot()
+
+  const button = await Selector(
+    "body > main-app > div > stock-inventory > div > form > div > button",
+  )
+
+  await t.expect(button.visible).ok()
 })
