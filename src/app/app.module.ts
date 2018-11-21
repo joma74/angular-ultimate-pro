8ed2b36d5bf39a53c59e811adc71d0820e1eae31
joma74 <dev-mails@gmx.net>
2018-11-22 00:27:07 +0100

    feat: add configurable preloading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from "@angular/core"
 import { HttpModule } from "@angular/http"
 import { BrowserModule } from "@angular/platform-browser"
-import { RouterModule, Routes } from "@angular/router"
+import {
+  PreloadingStrategy,
+  Route,
+  RouterModule,
+  Routes,
+} from "@angular/router"
 
 import { MailModule } from "./mail/mail.module"
 
+import { Observable } from "rxjs"
 import { AppComponent } from "./app.component"
+
+export class CustomPreload implements PreloadingStrategy {
+  public preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    return route.data && route.data.preload ? fn() : Observable.of(null)
+  }
+}
 
 export const ROUTES: Routes = [
   {
+    data: { preload: false },
     loadChildren: "./dashboard/dashboard.module#DashboardModule",
     path: "dashboard",
   },
   { path: "**", redirectTo: "mail/folder/inbox" },
 ]
 
+// tslint:disable-next-line:max-classes-per-file
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
@@ -22,7 +36,8 @@ export const ROUTES: Routes = [
     BrowserModule,
     HttpModule,
     MailModule,
-    RouterModule.forRoot(ROUTES),
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: CustomPreload }),
   ],
+  providers: [CustomPreload],
 })
 export class AppModule {}
