b43ecc0ee7dcefe018e039e7c327c6b908691eb3
joma74 <dev-mails@gmx.net>
2018-11-23 16:31:41 +0100

    test: add shallow tests for filesize pipe

--- src/app/filesize.pipe.spec.ts
+++ src/app/filesize.pipe.spec.ts
@@ -1,6 +1,64 @@
+import { Component } from "@angular/core"
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from "@angular/platform-browser-dynamic/testing"
 import { FileSizePipe } from "./filesize.pipe"
 
 describe("FileSizePipe", () => {
+  describe("Shallow FileSizePipe test", () => {
+    @Component({
+      template: `
+        Size: {{ size | filesize: suffix }}
+      `,
+    })
+    class TestComponent {
+      public suffix: string
+      public size = 123456789
+    }
+
+    let component: TestComponent
+    let fixture: ComponentFixture<TestComponent>
+    let el: HTMLElement
+
+    beforeAll(() => {
+      TestBed.resetTestEnvironment()
+      TestBed.initTestEnvironment(
+        BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting(),
+      )
+    })
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [FileSizePipe, TestComponent],
+      })
+      fixture = TestBed.createComponent(TestComponent)
+      component = fixture.componentInstance
+      el = fixture.nativeElement
+    })
+
+    it("should convert bytes to megabtyes", () => {
+      fixture.detectChanges()
+      expect(el.textContent).toContain("Size: 117.74MB")
+      component.size = 1029281
+      fixture.detectChanges()
+      expect(el.textContent).toContain("Size: 0.98MB")
+    })
+
+    it("should use the default extension when not supplied", () => {
+      fixture.detectChanges()
+      expect(el.textContent).toContain("Size: 117.74MB")
+    })
+
+    it("should override the extension when supplied", () => {
+      component.suffix = "myExt"
+      fixture.detectChanges()
+      expect(el.textContent).toContain("Size: 117.74myExt")
+    })
+  })
+
   describe("Isolate FileSizePipe test", () => {
     const pipe = new FileSizePipe()
 
