200dd0cf63d3fd3b72b96d937c7a754f4a729cb9
joma74 <dev-mails@gmx.net>
2018-12-20 13:01:15 +0100

    refactor: move stack trace setup out of polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,12 +1,3 @@
+import "core-js/es6/reflect"
 import "core-js/es7/reflect"
-// tslint:disable-next-line:no-var-requires
-require("zone.js/dist/zone")
-
-if (process.env.ENV === "production") {
-  // Production
-} else {
-  // Development and test
-  Error.stackTraceLimit = Infinity
-  // tslint:disable-next-line:no-var-requires
-  require("zone.js/dist/long-stack-trace-zone")
-}
+import "zone.js/dist/zone"
